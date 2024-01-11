56b0bfe64451c973f4367c56267fb5a4c70ae719
Jiwoo12345 <yahootomb370@gmail.com>
2024-01-11 15:14:47 +0100

    PHP

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php
+require "navbar.php";
+?>
+
+<h1>
+<?php
+require "database.php";
+?>
+</h1>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Title</title>
+</head>
+<body>
+<nav>
+    <img src="Images/logoipsum-230.svg" alt="">
+	<ul>
+        <li><a href="navbar.php">Link</a></li>
+        <li><a href="navbar.php">Link</a></li>
+        <li><a href="navbar.php">Link</a></li>
+        <li><a href="navbar.php">Link</a></li>
+        <li><a href="navbar.php">Link</a></li>
+	</ul>
+</nav>
+</body>
+</html>
