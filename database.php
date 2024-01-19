9f5c53e3d7437d2d049cfd076bb5ccae26223bc9
Jiwoo12345 <yahootomb370@gmail.com>
2024-01-19 15:02:45 +0100

    database

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
 try{
-$database = new PDO("mysql:host=localhost;dbname=chirpify", "root", "root");
+$database = new PDO("mysql:host=localhost;dbname=PHP project", "root", "root");
 $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 echo "Connected successfully";
 } catch(PDOException $e) {
